6044a3e14a47d73107512c6e832cd8718ae0c6b4
Tóth Róbert <Nekomajin42@users.noreply.github.com>
2020-01-30 18:39:41 +0100

    Update file-beolvasas.java

--- File beolvasás/file-beolvasas.java
+++ File beolvasás/file-beolvasas.java
@@ -8,40 +8,39 @@ import java.util.Scanner;
 
 public class Beolvasás {
 
-    public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws FileNotFoundException {
 
 		// 0. lépés -> A file megnyitása (a be.txt érettségizők adatait tartalmazza)
-        File be = new File("be.txt");
-        Scanner sc = new Scanner(be);
+		File be = new File("be.txt");
+		Scanner sc = new Scanner(be);
 
 		// 1. lépés
-        String sor;
+		String sor;
 
 		// 2. lépés
-        String[] tömb;
+		String[] tömb;
 
 		// 3. lépés
-        String név;
-		String nem;
+		String név, nem;
 		String[] születésidátum;
 		int év, hónap, nap;
 		boolean emelt;
 
 		// 4. lépés
-        Személy érettségiző;
+		Személy érettségiző;
 
 		// 5. lépés
-        ArrayList<Személy> érettségizők = new ArrayList<>();
+		ArrayList<Személy> érettségizők = new ArrayList<>();
 
-        while (sc.hasNextLine()) {
+		while (sc.hasNextLine()) {
 
 			// 1. lépés -> Egy sor beolvasása
-            sor = sc.nextLine();
+			sor = sc.nextLine();
 
 			// 2. lépés -> A sor feldarabolása a megadott határolókarater mentén
-            tömb = sor.split(";");
+			tömb = sor.split(";");
 
-            // 3. lépés -> Az adatok konvertálása a megfelelő típusra
+			// 3. lépés -> Az adatok konvertálása a megfelelő típusra
 			név = tömb[0];
 			nem = tömb[1].equals("0") ? "férfi" : "nő";
 			
@@ -63,13 +62,12 @@ public class Beolvasás {
 
 class Személy {
 
-    String név;
-	String nem;
+	String név, nem;
 	int év, hónap, nap;
 	boolean emelt;
 
-    public Utazas(String név, String nem, int év, int hónap, int nap, boolean emelt) {
-        this.név = név;
+	public Utazas(String név, String nem, int év, int hónap, int nap, boolean emelt) {
+		this.név = név;
 		this.nem = nem;
 		this.év = év;
 		this.hónap = hónap;
